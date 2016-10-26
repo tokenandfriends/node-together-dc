752841425f416ae651b3663ee8a79b727392402e
Kedwin Hernandez <tokenandfriends@gmail.com>
2016-10-25 21:50:32 -0400

    updated enviornment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const port = process.env.port || '8080';
-const host = process.env.host || '0.0.0.0';
+const port = process.env.PORT || '8080';
+const host = process.env.HOST || '0.0.0.0';
 const express = require ('express');
 const app = express();
 
